28b9ee22fcc07bb5e765866bdba12e7ffd268f56
Abin Meza <mukarabinaliamu@gmail.com>
2023-06-25 17:05:57 +0800

    saving to database

--- api.php
+++ api.php
@@ -4,10 +4,29 @@ require_once("classes/autoload.php");
 
 $DB = new Database();
 
-$data = file_get_contents("php://input");
-$myobject = json_decode($data);
+$DATA_RAW = file_get_contents("php://input");
+$DATA_OBJ = json_decode($DATA_RAW);
 
+//process the data
+if (isset($DATA_OBJ->data_type) && $DATA_OBJ->data_type == "signup") {
 
+    //signup
+    $data = false;
+    $data['userid'] = $DB->generate_id(20);
+    $data['username'] = $DATA_OBJ->username;
+    $data['email'] = $DATA_OBJ->email;
+    $data['password'] = $DATA_OBJ->password;
+    $data['date'] = $DATA_OBJ->date("Y-m-d H:i:s");
+
+    $query =  "insert into users (userid,username,email,password,date) values (:userid,:username,:email,:password,:date)";
+    $result = $DB->write($query, $data);
+
+    if ($result) {
+        echo "Your profile was created";
+    } else {
+        echo "Your profile was NOT created";
+    }
+}
 
 //json_decode(json) = JSON.parse()
 //json_encode(value) = JSON.strinfigy()
